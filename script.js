1c275d370630525c9714c5fae2030751430800f7
Krackalackin <44794979+kcarri@users.noreply.github.com>
2019-12-02 00:16:10 -0600

    Bug fix

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ Fall2019*/
 // Firstly,
 function askName(yourName) {
   var yourName = prompt("What is your name?", "Kat");
+  if (Kat) {
+console.log("That's my name!");
+} else {
+console.log("Wilkommen!");
+}
   document.getElementById("showName").innerHTML = text;
   return yourName;
 }
@@ -51,3 +56,32 @@ function keyPress() {
     bugs.jump();
   }
 }
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
